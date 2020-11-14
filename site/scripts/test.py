4fc9ec5592079aaa66eef48a5382bdeb2306f8b6
iz0301 <isaaczachmann@gmail.com>
2020-11-14 18:23:29 +0000

    added script to handl form info

--- site/scripts/test.py
+++ site/scripts/test.py
@@ -2,10 +2,15 @@
 
 import cgi
 import cgitb
-
+import sys
 
 cgitb.enable()
 print("Content-Type: text/html\n\n")
 
-print('hi clark')
+form = cgi.FieldStorage()
+if "fname" not in form or "lname" not in form:
+    print("<H1>Error</H1>")
+    print("Please fill in the name and addr fields.")
+    sys.exit(0)
+print("Hello " + form['fname'].value +" "+ form['lname'].value)
 
