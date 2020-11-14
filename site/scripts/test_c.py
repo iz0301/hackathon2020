91c6ac74fd214747bf437573cdd231f146fee698
Clark <clarkvan33@gmail.com>
2020-11-14 13:22:14 -0500

    Changed some filenames and started to test out CGI scripts

--- site/scripts/test_c.py
+++ site/scripts/test_c.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3 -u
 
-import cgi
-import cgitb
-
+import cgi, cgitb
 
 cgitb.enable()
-print("Content-Type: text/html\n\n")
+# Create instance of FieldStorage 
+form = cgi.FieldStorage() 
 
-print('hi clark')
+# Get data from fields
+first_name = form.getvalue('First Name')
+last_name  = form.getvalue('Last Name')
+
+print ("Content-type:text/html\r\n\r\n")
+print ("<html>")
+print ("<head>")
+print ("<title>Hello - Second CGI Program</title>")
+print ("</head>")
+print ("<body>")
+print ("<h2>Hello %s %s</h2>" % (first_name, last_name))
+print ("</body>")
+print ("</html>")
