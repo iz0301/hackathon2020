d6d172594f8ddbf2435efcee07417d81943ef9b9
Clark <clarkvan33@gmail.com>
2020-11-14 18:20:57 -0500

    signup script, with straightaway login tacked on.

--- site/scripts/signup.py
+++ site/scripts/signup.py
@@ -8,6 +8,8 @@ cgitb.enable()
 
 form = cgi.FieldStorage()
 if "username" not in form or "password" not in form:
+    print('Set-Cookie: username=');
+    print('Set-Cookie: password=');
     print("Content-Type: text/html\n\n")
     sys.exit(0)
 
@@ -27,4 +29,28 @@ def save_password(obj, filename):
 #If the username folder is not created yet, create it, and the password.txt file:
 if not os.path.exists('/var/www/hack2020/'+username+'/'):
     os.mkdir('/var/www/hack2020/'+username+'/')
-    save_password(password, '/var/www/hack2020/'+username+'/'+'password.txt'')
+    save_password(password, '/var/www/hack2020/'+username+'/'+'password.txt')
+ else:
+ 	print('You are already signed up. Contact an administrator if you forgot your password.')
+
+#Now, log them in straight away:
+#A check: print("Hello " + form['username'].value +" "+ form['lastname'].value)
+
+#Check the username and password against a user folder:
+passwordfilename = '/var/www/hack2020/'+username+'/'+'password.txt'
+with open(passwordfilename, 'r') as file:
+    password_from_file = file.read()
+
+if password_from_file.strip() == password.strip():
+    #print("Login successful, " + username_in)
+    print('Set-Cookie: logged_in=1');
+    print('Set-Cookie: username=' + username);
+    print('Set-Cookie: password=' + password.strip());
+else:
+    #print("Login unsuccessful, " + username_in)
+    print('Set-Cookie: logged_in=0');
+    print('Set-Cookie: username=' + username);
+    print('Set-Cookie: password=' + password.strip());
+
+
+print("Content-Type: text/html\n\n")
