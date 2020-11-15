b68d1b95340da8acad53d1731ab17d5c94f26bbf
Clark <clarkvan33@gmail.com>
2020-11-14 23:09:05 -0500

    testing

--- site/scripts/follow.py
+++ site/scripts/follow.py
@@ -4,10 +4,12 @@ import cgitb
 import json
 
 cgitb.enable()
-
+print('hello world')
+"""
 form = cgi.FieldStorage()
 print(form)
 value = form.getlist("username")
 following = ",".join(value)
 
 print("Content-Type: text/html\n\n")
+"""
