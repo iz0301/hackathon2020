34ba55122084c3c48ee5646d91640ecef031d5aa
Clark <clarkvan33@gmail.com>
2020-11-15 10:58:09 -0500

    changed variable definition order to avoid an error early in the script

--- site/scripts/interestCalc.py
+++ site/scripts/interestCalc.py
@@ -20,6 +20,33 @@ don_or_loan = form['don_or_loan'].value.lower()
 repayLength = form['repayLength'].value
 to = form['to'].value
 
+#------------------------------------------------------------------------------
+#Aside: Check all cookies
+#Read the cookies that have been established:
+handler = {} # A dictionary which will house the cookie names and cookie values
+if 'HTTP_COOKIE' in os.environ:
+    cookies = os.environ['HTTP_COOKIE']
+    cookies = cookies.split('; ')
+    for cookie in cookies:
+        cookie = cookie.split('=')
+        handler[cookie[0]] = cookie[1]
+#handler now has all 3 cookies we want.
+#Now, do checks with these cookies:
+username = ""
+if 'logged_in' in handler and handler['logged_in'] == '1': #(Checked if logged in)
+    with open('/var/www/hack2020/'+handler['username']+'/'+'password.txt','r') as file:
+        fp = file.read()
+
+    if handler['password'].strip() == fp.strip():
+        username = handler['username']
+    else:
+        print('Wrong password!')
+        quit()
+else:
+    print('not logged in')
+    quit()
+#------------------------------------------------------------------------------
+
 #Convert the numerical values to floats:
 amt_money = float(amt_money)
 interestPercent = float(interestPercent)
@@ -85,32 +112,6 @@ def calc_amounts(amt_money, interestPercent, don_or_loan, repayLength):
     loan_stats = {'amt_money': amt_money, 'interestRate': interestRate, 'totalOwed': totalOwed, 'intervalPay': intervalPay, 'repayLength': repayLength}
     return loan_stats
 
-#------------------------------------------------------------------------------
-#Aside: Check all cookies
-#Read the cookies that have been established:
-handler = {} # A dictionary which will house the cookie names and cookie values
-if 'HTTP_COOKIE' in os.environ:
-    cookies = os.environ['HTTP_COOKIE']
-    cookies = cookies.split('; ')
-    for cookie in cookies:
-        cookie = cookie.split('=')
-        handler[cookie[0]] = cookie[1]
-#handler now has all 3 cookies we want.
-#Now, do checks with these cookies:
-username = ""
-if 'logged_in' in handler and handler['logged_in'] == '1': #(Checked if logged in)
-    with open('/var/www/hack2020/'+handler['username']+'/'+'password.txt','r') as file:
-        fp = file.read()
-
-    if handler['password'].strip() == fp.strip():
-        username = handler['username']
-    else:
-        print('Wrong password!')
-        quit()
-else:
-    print('not logged in')
-    quit()
-#------------------------------------------------------------------------------
 calc_amounts(amt_money, interestPercent, don_or_loan, repayLength)
 #Now that we have all the stats and the interest for the loan, save the data to the correct files:
 #For the loaner:
